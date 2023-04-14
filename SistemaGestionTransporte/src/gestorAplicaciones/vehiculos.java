3fe91afcba214dfb55a27bd3845af5b6fa5d9ea6
Camilord01 <124930730+Milord01@users.noreply.github.com>
2023-04-14 15:47:53 -0500

    Prueba

---
+++ SistemaGestionTransporte/src/gestorAplicaciones/vehiculos.java
@@ -0,0 +1,14 @@
+package gestorAplicaciones;
+
+public class vehiculos {
+
+	public vehiculos() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
