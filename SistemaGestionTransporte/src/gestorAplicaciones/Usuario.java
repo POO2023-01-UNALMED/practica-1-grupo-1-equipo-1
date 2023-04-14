867336e582376a795b71a2a91dcfd3e5dd7e0338
Melian19 <lgomezgr@unal.edu.co>
2023-04-14 15:55:23 -0500

    first

---
+++ SistemaGestionTransporte/src/gestorAplicaciones/Usuario.java
@@ -0,0 +1,24 @@
+package gestorAplicaciones;
+
+public class Usuario {
+	
+	//atributos
+	private String nombre;
+	private String clave;
+	private int documento;
+	private float saldo;
+	private Pedido pedido;
+	private String tipoUsuario;
+	
+	//constructor
+	
+	public Usuario (String nombreUsuario, String nuevaClave, int id) {
+		
+	}
+	
+	//metodod get y set
+	
+	//metodos
+	
+
+}
