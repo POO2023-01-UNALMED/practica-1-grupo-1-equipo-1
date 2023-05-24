b271bc876428cbc530cad653eb647cdb5eeca87f
Julian Salazar <jusalazard@unal.edu.co>
2023-05-23 23:40:28 -0500

    Algoritmo de dinkstra en clase pedido

--- SistemaGestionTransporte/src/gestorAplicaciones/pais/Ciudad.java
+++ SistemaGestionTransporte/src/gestorAplicaciones/pais/Ciudad.java
@@ -38,86 +38,36 @@ public class Ciudad {
 
     }
 
-    //metodos
-    public Pair<String,Double> planificarRuta(String destino){
-        /*
-        este metodo implementa el algoritmo de dijkstra para encontrar el camino mas corto entre dos
-        ciudades.
-         */
-
-        Map<String, Pair<Boolean,String>> ciudadesVisitadas = new HashMap<>();
-        this.inicializarCiudadesVisitadas(ciudadesVisitadas);
-        Map<String, Double> costos = new HashMap<>(this.conexiones);
-        this.inicializarCostos(costos);
-        PriorityQueue<Pair<String, Double>> queue = new PriorityQueue<>(Comparator.comparing(Pair::getValue));
-        queue.offer(new Pair<>(this.nombre,0d));
-        String nombre;
-        double distancia;
-        ciudadesVisitadas.put(this.nombre,new Pair<>(true,"-"));
-        Ciudad ciudad;
-
-
-        while (!queue.isEmpty()){
-            nombre = queue.poll().getKey();
-            ciudadesVisitadas.put(nombre,new Pair<>(true,ciudadesVisitadas.get(nombre).getValue()));
-
-            if(this.nombrePais.equals("Colombia")) {
-                ciudad = Pais.COLOMBIA.getCiudad(nombre);
-            }
-            else if(this.nombrePais.equals("Ecaudor")){
-                ciudad = Pais.ECUADOR.getCiudad(nombre);
-            }
-            else{
-                ciudad = Pais.PANAMA.getCiudad(nombre);
-            }
-
-            if (nombre.equals(destino)) break;
-            assert ciudad != null;
-            for (Map.Entry<String, Double> map : ciudad.conexiones.entrySet()){
-
-                if(map.getValue() != -1 && !ciudadesVisitadas.get(map.getKey()).getKey()){
-                    distancia = costos.get(nombre) + ciudad.conexiones.get(map.getKey());
-                    System.out.println(costos.get(map.getKey()));
-                    if(distancia <= costos.get(map.getKey())){
-                        costos.put(map.getKey(), distancia);
-                        queue.offer(new Pair<>(map.getKey(), distancia));
-                        ciudadesVisitadas.put(map.getKey(),new Pair<>(false,nombre));
-                    }
-                }
-            }
-        }
-        distancia = costos.get(destino);
-
-        return new Pair<>(this.getRuta(destino,ciudadesVisitadas), distancia);
-
+    public String getNombrePais() {
+        return nombrePais;
     }
 
-    private void inicializarCiudadesVisitadas(Map<String, Pair<Boolean,String>> ciudadesVisitadas){
+    public void setNombrePais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
+
+    public Map<String, Double> getConexiones() {
+        return conexiones;
+    }
+
+    public void setConexiones(Map<String, Double> conexiones) {
+        this.conexiones = conexiones;
+    }
+
+    public void inicializarCiudadesVisitadas(Map<String, Pair<Boolean, String>> ciudadesVisitadas){
         //iniciar el map CiudadesVisitadas.
         for (Map.Entry<String, Double> map : conexiones.entrySet()) {
             ciudadesVisitadas.put(map.getKey(),new Pair<>(false,"-"));
         }
     }
 
-    private void inicializarCostos(Map<String, Double> costos){
-        //iniciaar el map costos con el valor maximo
+    public void inicializarCostos(Map<String, Double> costos){
+        //iniciaar el map costos con el valor maximo de Double
         for (Map.Entry<String, Double> map : costos.entrySet()) {
             if(map.getValue() == -1){
                 map.setValue(Double.MAX_VALUE);
             }
         }
-    }
-
-    private String getRuta(String destino, Map<String, Pair<Boolean,String>> ciudadesVisitadas){
-        //obtener la ruta de una ciudad A a una ciudad B con el el mapa de ciudades visitadas
-        StringBuilder procedencia = new StringBuilder(destino);
-        destino = ciudadesVisitadas.get(destino).getValue();
-        while (!destino.equals("-")){
-            procedencia.append(" - ").append(destino);
-            destino = ciudadesVisitadas.get(destino).getValue();
-        }
-
-        return procedencia.reverse().toString();
     }
 
     @Override
--- SistemaGestionTransporte/src/gestorAplicaciones/producto/Pedido.java
+++ SistemaGestionTransporte/src/gestorAplicaciones/producto/Pedido.java
@@ -1,8 +1,10 @@
 package gestorAplicaciones.producto;
 
+import gestorAplicaciones.pais.Ciudad;
+import gestorAplicaciones.pais.Pais;
 import gestorAplicaciones.util.Pair;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Pedido {
 
@@ -13,7 +15,7 @@ public class Pedido {
     private double peso_carga;
     ArrayList<Producto> productos;
     private double costoPedido;
-    private String pais;
+    private Pais pais;
     private String estado;
 
     //constructor
@@ -70,11 +72,11 @@ public class Pedido {
         this.costoPedido = costoPedido;
     }
 
-    public String getPais() {
+    public Pais getPais() {
         return pais;
     }
 
-    public void setPais(String pais) {
+    public void setPais(Pais pais) {
         this.pais = pais;
     }
 
@@ -88,7 +90,73 @@ public class Pedido {
 
     //metodos
     public ArrayList<Pair<String,Double>> calcularRuta(){
-        return null;
+        /*
+        este metodo implementa el algoritmo de dijkstra para encontrar el camino mas corto entre dos
+        las ciudades origen y destino.
+         */
+
+        //obtener el objeto de la ciudad destino
+        Ciudad ciudadPartida = this.pais.getCiudad(this.origen);
+
+        Map<String, Pair<Boolean,String>> ciudadesVisitadas = new HashMap<>();
+        assert ciudadPartida != null;
+        ciudadPartida.inicializarCiudadesVisitadas(ciudadesVisitadas);
+
+        Map<String, Double> costos = new HashMap<>(ciudadPartida.getConexiones());
+        ciudadPartida.inicializarCostos(costos);
+
+        PriorityQueue<Pair<String, Double>> queue = new PriorityQueue<>(Comparator.comparing(Pair::getValue));
+        queue.offer(new Pair<>(ciudadPartida.getNombre(),0d));
+
+        String nombre;
+        double distancia;
+        ciudadesVisitadas.put(ciudadPartida.getNombre(),new Pair<>(true,"-"));
+        Ciudad ciudad;
+
+        while (!queue.isEmpty()){
+            nombre = queue.poll().getKey();
+            ciudadesVisitadas.put(nombre,new Pair<>(true,ciudadesVisitadas.get(nombre).getValue()));
+
+            ciudad = this.pais.getCiudad(nombre);
+
+            if (nombre.equals(this.destino)) break;
+            assert ciudad != null;
+            for (Map.Entry<String, Double> map : ciudad.getConexiones().entrySet()){
+
+                if(map.getValue() != -1 && !ciudadesVisitadas.get(map.getKey()).getKey()){
+                    distancia = costos.get(nombre) + ciudad.getConexiones().get(map.getKey());
+
+                    if(distancia <= costos.get(map.getKey())){
+                        costos.put(map.getKey(), distancia);
+                        queue.offer(new Pair<>(map.getKey(), distancia));
+                        ciudadesVisitadas.put(map.getKey(),new Pair<>(false,nombre));
+                    }
+                }
+            }
+        }
+        return  this.getRuta(this.getCamino(this.destino,ciudadesVisitadas),costos);
+    }
+
+    private ArrayList<String> getCamino(String destino, Map<String, Pair<Boolean, String>> ciudadesVisitadas){
+        //obtener la ruta de una ciudad A a una ciudad B con el el mapa de ciudades visitadas
+        ArrayList<String> procedencia = new ArrayList<String>();
+        procedencia.add(destino);
+        destino = ciudadesVisitadas.get(destino).getValue();
+        while (!destino.equals("-")){
+            procedencia.add(destino);
+            destino = ciudadesVisitadas.get(destino).getValue();
+        }
+
+        Collections.reverse(procedencia);
+        return procedencia;
+    }
+
+    private ArrayList<Pair<String,Double>> getRuta(ArrayList<String> camino, Map<String, Double> costos){
+        ArrayList<Pair<String,Double>> ruta = new ArrayList<Pair<String,Double>>();
+        for(String ciudad : camino){
+            ruta.add(new Pair<>(ciudad,costos.get(ciudad)));
+        }
+        return ruta;
     }
 
     public double CalcularPeso(){
@@ -100,5 +168,16 @@ public class Pedido {
     }
     public void asignarConductor(){
 
+    }
+
+    public static void main(String[] args){
+        Pedido pedido= new Pedido();
+        pedido.setPais(Pais.COLOMBIA);
+        pedido.setOrigen("T");
+        pedido.setDestino("B");
+        ArrayList<Pair<String,Double>> ruta = pedido.calcularRuta();
+        for(Pair<String,Double> ciudad : ruta){
+            System.out.println(ciudad.getKey()+"\t"+ciudad.getValue());
+        }
     }
 }
