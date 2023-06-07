2e0c626cf74489372c9fa203837634a6acb74615
Julian Salazar <jusalazard@unal.edu.co>
2023-06-07 11:11:29 -0500

    adelantos ventana de inicio

--- python/SistemaGestionTransporte/src/uiMain/inicio.py
+++ python/SistemaGestionTransporte/src/uiMain/inicio.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.font import Font
 
 
 class Inicio(tk.Frame):
@@ -18,4 +19,16 @@ class Inicio(tk.Frame):
         self.p5.place(x=0, y=0)
         self.p6 = tk.Frame(self.p2, bg="brown", width=455, height=350)
         self.p6.place(x=0, y=180)
+        self.bienvenida()
+        self.desarrolladores()
 
+    def bienvenida(self):
+        font = Font(size=40)
+        tk.Label(self.p3, text="Bienvenidos a \nTransportes Ltda", font=font).place(x=25, y=30)
+        tk.Button(self.p4, text="ingresar", font=font, command=self.tipoSesion).place(x=100, y=120)
+
+    def desarrolladores(self):
+        pass
+
+    def tipoSesion(self):
+        self.p4.winfo_children()[0].destroy()
