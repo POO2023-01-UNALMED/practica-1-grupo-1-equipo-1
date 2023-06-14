8995c746b830d1bac5b19eaa772a64754a4d574f
Michael Garcia <124626393+Mgarciaqu@users.noreply.github.com>
2023-06-13 23:56:42 -0500

    Update usuario.py

--- python/SistemaGestionTransporte/src/gestorAplicaciones/entidades/usuario.py
+++ python/SistemaGestionTransporte/src/gestorAplicaciones/entidades/usuario.py
@@ -2,118 +2,80 @@ import pickle
 
 class Usuario:
     usuarios = []
-
+    
     def __init__(self, nombre, clave, id, correo):
         self.nombre = nombre
         self.clave = clave
         self.ID = id
         self.correo = correo
         Usuario.usuarios.append(self)
-
+    
     @classmethod
-    def get_usuarios(cls):
-        return cls.usuarios
-
-    @classmethod
-    def set_usuarios(cls, usuarios):
-        cls.usuarios = usuarios
-
-    def get_nombre(self):
-        return self.nombre
-
-    def get_clave(self):
-        return self.clave
-
-    def set_clave(self, clave):
-        self.clave = clave
-
-    def get_ID(self):
-        return self.ID
-
-    def get_correo(self):
-        return self.correo
-
-    def set_correo(self, correo):
-        self.correo = correo
-
-    @staticmethod
-    def crear_usuario(nombre, clave, id, correo):
-        return Usuario(nombre, clave, id, correo)
-
-    def comprobar_usuario(self, nombre, clave):
+    def crearUsuario(cls, nombre, clave, id, correo):
+        return cls(nombre, clave, id, correo)
+    
+    def comprobarUsuario(self, nombre, clave):
         return self.nombre == nombre and self.clave == clave
-
-    def comprobar_usuario_id(self, id, clave):
+    
+    def comprobarUsuario(self, id, clave):
         return self.ID == id and self.clave == clave
-
-    def comprobar_nombre(self, nombre):
+    
+    def comprobarNombre(self, nombre):
         return self.nombre == nombre
-
-    def comprobar_ID(self, id):
+    
+    def comprobarID(self, id):
         return self.ID == id
-
-    def comprobar_correo(self, correo):
+    
+    def comprobarCorreo(self, correo):
         return self.correo == correo
-
+    
     @staticmethod
-    def is_nombre_valido(nombre):
-        if nombre is None or not all(c == ' ' or c.isalpha() for c in nombre):
-            print("El nombre solo debe tener caracteres alfanuméricos.")
+    def isNombreValido(nombre):
+        if nombre is None or not nombre.isalpha():
+            print("El nombre solo debe tener caracteres alfabéticos.")
             return False
         for usuario in Usuario.usuarios:
-            if usuario.comprobar_nombre(nombre):
+            if usuario.comprobarNombre(nombre):
                 print("Nombre ya registrado.")
                 return False
         return True
-
+    
     @staticmethod
-    def is_ID_valido(id):
+    def isIDValido(id):
         if id is None or not id.isdigit():
             print("La identificación debe contener solo caracteres numéricos.")
             return False
         for usuario in Usuario.usuarios:
-            if usuario.comprobar_ID(int(id)):
+            if usuario.comprobarID(int(id)):
                 print("Número de identificación ya registrado.")
                 return False
         return True
-
+    
     @staticmethod
-    def is_correo_valido(correo):
-        if '@' not in correo or correo[-1] == '@':
+    def isCorreoValido(correo):
+        if '@' not in correo or correo.endswith('@'):
             print("Correo no válido.")
             return False
         for usuario in Usuario.usuarios:
-            if usuario.comprobar_correo(correo):
+            if usuario.comprobarCorreo(correo):
                 print("Correo ya registrado.")
                 return False
         return True
-
+    
     def __str__(self):
         return f"\nnombre:\t{self.nombre}\nid:\t{self.ID}\ncorreo:\t{self.correo}\n"
 
-    def __repr__(self):
-        return str(self)
+    # Métodos para serialización y deserialización de objetos
+    def guardarDatos(self, archivo):
+        with open(archivo, 'wb') as f:
+            pickle.dump(Usuario.usuarios, f)
+    
+    @staticmethod
+    def cargarDatos(archivo):
+        with open(archivo, 'rb') as f:
+            Usuario.usuarios = pickle.load(f)
+
+    # Atributo y anotación para serialización
+    serialVersionUID = 1L
 
 
-# Clase Empleado
-class Empleado(Usuario):
-    empleados = []
-
-    def __init__(self, nombre, clave, id, correo, estatus_activo, pais, ciudad_actual):
-        super().__init__(nombre, clave, id, correo)
-        self.estatus_activo = estatus_activo
-        self.disponible = True
-        self.pais = pais
-        self.ciudad_actual = ciudad_actual
-        Empleado.empleados.append(self)
-
-    @classmethod
-    def get_empleados(cls):
-        return cls.empleados
-
-    @classmethod
-    def set_empleados(cls, empleados):
-        cls.empleados = empleados
-
-    def is_estatus_activo(self):
-        return self.estatus_act
