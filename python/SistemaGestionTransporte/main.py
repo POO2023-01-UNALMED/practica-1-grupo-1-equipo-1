cb14482d1f9d23a066dee9ffe7dc6ff98f3d18d3
Julian Salazar <jusalazard@unal.edu.co>
2023-06-18 16:01:51 -0500

    verificar PIL

--- python/SistemaGestionTransporte/main.py
+++ python/SistemaGestionTransporte/main.py
@@ -5,20 +5,23 @@ from src.uiMain.uiMain import UiMain
 from tkinter import messagebox
 import importlib
 
-# Verificar si PIL está instalado
-try:
-    importlib.import_module('PIL')
-except ImportError:
-    try:
-        import pip
 
-        pip.main(['install', 'Pillow'])
+# Verificar si PIL está instalado
+def verificcarPIL():
+    try:
+        importlib.import_module('PIL')
     except ImportError:
-        messagebox.showerror('Pillow', "No se pudo instalar PIL. Asegúrate de tener pip instalado.")
-        exit()
+        try:
+            import pip
+
+            pip.main(['install', 'Pillow'])
+        except ImportError:
+            messagebox.showerror('Pillow', "No se pudo instalar PIL. Asegúrate de tener pip instalado.")
+            exit()
 
 
 if __name__ == '__main__':
+    verificcarPIL()
     # CargarInformacion.cargarUsuarios()
     # CargarInformacion.cargarEmpleados()
     # CargarInformacion.cargarCamiones()
